dae86a66753ba47cfe0d889d941a9afdf50dd377
eziraa <ezratgab@gmail.com>
2024-04-02 17:08:45 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:technews/log_in.dart';
 import 'package:technews/on_boarding_1.dart';
+import 'package:technews/select_topic.dart';
+import 'package:technews/sign_up.dart';
 
 void main() {
   runApp(
@@ -7,6 +10,6 @@ void main() {
         title: 'Tech News App',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.grey),
-        home: const OnBoarding1()),
+        home: SelectTopics()),
   );
 }
