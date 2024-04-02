28ac660614a6f32da001d8073716bc12ac0478bd
eziraa <ezratgab@gmail.com>
2024-04-02 14:34:34 +0300

    adding the sign up page

---
+++ lib/sign_up.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:technews/logo.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(50),
+        child: Column(
+          children: [
+            const Logo(),
+            const SizedBox(height: 30),
+            const Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Username", labelStyle: TextStyle()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Email", labelStyle: TextStyle()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Password", labelStyle: TextStyle()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Re-enter Password", labelStyle: TextStyle()),
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50),
+              color: Colors.blueAccent,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0))),
+                onPressed: null,
+                child: const Text(
+                  "Sign up",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
