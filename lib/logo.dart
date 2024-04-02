ad3c8b56cecf23b0fe3ee0b549202ad8399b6b44
eziraa <ezratgab@gmail.com>
2024-04-02 14:07:41 +0300

    Creating the logo component

---
+++ lib/logo.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Image.asset('assets/images/logo.png'),
+          const CircleAvatar(
+            child: Icon(Icons.newspaper),
+          ),
+          RichText(
+            text: const TextSpan(children: [
+              TextSpan(
+                text: "Touch",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
+                ),
+              ),
+              TextSpan(
+                text: "Tech",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
