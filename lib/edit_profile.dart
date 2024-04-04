1faf73f1e8288c467f685d5444bfdd12008eded2
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-04 22:35:33 +0300

    edit profie added

---
+++ lib/edit_profile.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Edit your profile',
+            style: TextStyle(fontSize: 20),
+          ),
+          leading: const IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onPressed: null,
+          ),
+          actions: const [
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.black,
+                ))
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.only(left: 15, top: 20, right: 15),
+          child: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+              },
+              child: ListView(
+                children: [
+                  Center(
+                      child: Stack(children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 4, color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.1)),
+                          ],
+                          shape: BoxShape.circle,
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  "https://s.france24.com/media/display/e6279b3c-db08-11ee-b7f5-005056bf30b7/w:1024/p:16x9/news_en_1920x1080.jpg"))),
+                    ),
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(width: 4, color: Colors.white),
+                              color: Colors.blue),
+                          child: const Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        ))
+                  ])),
+                  const SizedBox(height: 30),
+                  Container(
+                    margin: const EdgeInsets.only(top: 50),
+                    child: const Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Username", labelStyle: TextStyle()),
+                        ),
+                        SizedBox(height: 10),
+                        SizedBox(height: 10),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "FullName", labelStyle: TextStyle()),
+                        ),
+                        SizedBox(height: 10),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Email", labelStyle: TextStyle()),
+                        ),
+                        SizedBox(height: 10),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Phone Number",
+                              labelStyle: TextStyle()),
+                        ),
+                        SizedBox(height: 10),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+        ));
+  }
+}
