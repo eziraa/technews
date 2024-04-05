e7a47c0ea3dfc4400b3a6cb561bf7a6c30114024
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-05 22:39:09 +0300

    updated main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:technews/choose_your_sources.dart';
+
 import 'package:technews/log_in.dart';
 import 'package:technews/on_boarding_1.dart';
 import 'package:technews/select_topic.dart';
-=======
-import 'package:technews/edit_profile.dart';
->>>>>>> 191efa7ddaeb21357591824263a4f1f3c4f94caa
+
 
 void main() {
   runApp(
     MaterialApp(
-<<<<<<< HEAD
       title: 'Tech News App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.grey),
@@ -21,12 +17,6 @@ void main() {
         '/log_in': (context) => const LogInPage(),
         '/select_topic': (context) => const SelectTopics(),
       },
-    ),
-=======
-        title: 'Tech News App',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.grey),
-        home: const EditProfile()),
->>>>>>> 191efa7ddaeb21357591824263a4f1f3c4f94caa
-  );
+    ));
+
 }
