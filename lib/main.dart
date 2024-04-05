099a703c7f23534aa086b2ec9989577c7ee2be42
eziraa <ezratgab@gmail.com>
2024-04-05 22:43:17 +0300

    Adding more routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,26 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:technews/choose_your_sources.dart';
+import 'package:technews/home_page.dart';
 import 'package:technews/log_in.dart';
 import 'package:technews/on_boarding_1.dart';
 import 'package:technews/select_topic.dart';
-=======
 import 'package:technews/edit_profile.dart';
->>>>>>> 191efa7ddaeb21357591824263a4f1f3c4f94caa
 
 void main() {
   runApp(
     MaterialApp(
-<<<<<<< HEAD
       title: 'Tech News App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.grey),
-      initialRoute: '/',
+      initialRoute: '/home_page',
       routes: {
         '/': (context) => const OnBoarding1(),
         '/log_in': (context) => const LogInPage(),
+        '/home_page': (context) => const HomePage(),
         '/select_topic': (context) => const SelectTopics(),
+        '/choose_news_sources': (context) => const ChooseNewsSources(),
+        '/edit_profile': (context) => const EditProfile(),
       },
     ),
-=======
-        title: 'Tech News App',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.grey),
-        home: const EditProfile()),
->>>>>>> 191efa7ddaeb21357591824263a4f1f3c4f94caa
   );
 }
