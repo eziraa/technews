3729da206869549f205efb93f28650f1c314f5b9
eziraa <ezratgab@gmail.com>
2024-04-05 21:10:44 +0300

    Adding routes to the material app to easily route from one page to other page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:technews/choose_your_sources.dart';
+import 'package:technews/log_in.dart';
+import 'package:technews/on_boarding_1.dart';
 import 'package:technews/select_topic.dart';
 
 void main() {
   runApp(
     MaterialApp(
-        title: 'Tech News App',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.grey),
-        home: const SelectTopics()),
+      title: 'Tech News App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.grey),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const OnBoarding1(),
+        '/log_in': (context) => const LogInPage(),
+        '/select_topic': (context) => const SelectTopics(),
+      },
+    ),
   );
 }
-
