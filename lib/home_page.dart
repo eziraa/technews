ff57434f8aa4f49eb7e1853c4f0af83bfd2ca414
eziraa <ezratgab@gmail.com>
2024-04-07 22:22:31 +0300

    Adding sections of home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,16 +10,140 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CustomWidget customWidget = CustomWidget();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomWidget().getAppBar(context),
+      appBar: customWidget.getAppBar(context),
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: Column(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [CustomWidget().getSearchBox()]),
+            children: [
+              customWidget.getSearchBox(),
+              _getTrending(),
+              _getNews(),
+              _getANews(),
+              _getANews(),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _getTrending() {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              customWidget.getBoldText("Trending", size: 24),
+              Expanded(
+                child: Container(),
+              ),
+              TextButton(
+                child: customWidget.blurredText("See All"),
+                onPressed: () {},
+              )
+              // Ico
+            ],
+          ),
+          const SizedBox(
+            height: 7,
+          ),
+          customWidget.getImage("assets/images/1.jpg"),
+          customWidget.blurredText("Europe"),
+          customWidget.getNormalText("Russian warship : Moskava sinks  ",
+              size: 24),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              customWidget.getProfileImage("assets/images/bbc.jpg"),
+              const SizedBox(
+                width: 5,
+              ),
+              customWidget.getBoldText("BBC News", color: Colors.black54),
+              const SizedBox(
+                width: 20,
+              ),
+              const Icon(
+                Icons.access_time,
+                size: 20,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              customWidget.blurredText("4hour ago")
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getNews() {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 10, bottom: 0),
+          child: Row(
+            children: [
+              customWidget.getBoldText("Latest", size: 24),
+              Expanded(
+                child: Container(),
+              ),
+              TextButton(
+                child: customWidget.blurredText("See All"),
+                onPressed: () {},
+              )
+              // Ico
+            ],
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(children: [
+            for (String item in [
+              'Artificial Intelligence (AI)',
+              'Machine Learning',
+              'Data Science',
+              'Blockchain and Cryptocurrency',
+              'Cybersecurity',
+              'Cloud Computing',
+              'Internet of Things (IoT)',
+              '5G and Connectivity',
+              'Augmented Reality (AR) and Virtual Reality (VR)',
+              'Gaming',
+              'Mobile Technology',
+              'Hardware',
+              'Software Development',
+              'Tech Policy and Regulation',
+              'Tech Business and Startups',
+              'Tech Events and Conferences'
+            ])
+              Row(
+                children: [
+                  TextButton(
+                    child: customWidget.blurredText(item),
+                    onPressed: () {},
+                  ),
+                  const SizedBox(width: 3)
+                ],
+              )
+          ]),
+        ),
+      ],
+    );
+  }
+
+  Widget _getANews() {
+    return Row(
+      children: [customWidget.getMediumImage("assets/images/6.jpg")],
     );
   }
 }
