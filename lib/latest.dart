8523aabbeb4ac0b0489846499655f3a8ae0e300e
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-08 00:44:40 +0300

    updated

--- lib/latest.dart
+++ lib/latest.dart
@@ -11,34 +11,34 @@ class _LatestState extends State<Latest> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const SizedBox(
-          child: Text(
-            'Latest News',
-            textAlign: TextAlign.left,
+        appBar: AppBar(
+          title: const SizedBox(
+            child: Text(
+              'Latest News',
+              textAlign: TextAlign.left,
+            ),
           ),
-        ),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-          IconButton(
+          leading: IconButton(
             icon: const Icon(
-              Icons.notifications,
+              Icons.arrow_back,
               color: Colors.black,
             ),
             onPressed: () {
-              
+              Navigator.pop(context);
             },
           ),
-        ],
-      ),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.notifications,
+                color: Colors.black,
+              ),
+            onPressed: () {
+              
+            },
+            ),
+          ],
+        ),
     );
   }
 }
