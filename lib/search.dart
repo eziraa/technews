a1de90777b1e1b39c0dc410fd7e4cef35c98f861
eziraa <ezratgab@gmail.com>
2024-04-08 23:38:22 +0300

    Adding the search option functionality

--- lib/search.dart
+++ lib/search.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:technews/CustomWidget.dart';
+
+class SearchOptions {
+  String? type;
+  String? task;
+  SearchOptions({required this.type, required this.task});
+}
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -10,13 +17,28 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   CustomWidget customWidget = CustomWidget();
+  List<SearchOptions> options = [];
+  SearchOptions activeOption = SearchOptions(type: 'News', task: '');
+  @override
+  void initState() {
+    options = [];
+    options.add(SearchOptions(type: 'News', task: 'Save'));
+    options.add(SearchOptions(type: "Topics", task: "Add"));
+    options.add(SearchOptions(type: "Source", task: "Follow"));
+    activeOption = options[0];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customWidget.getAppBar(context),
       body: Column(
-        children: [
-          customWidget.getSearchBox(context),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: customWidget.getSearchBox(context),
+          ),
           Expanded(
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
@@ -60,29 +82,36 @@ class _SearchPageState extends State<SearchPage> {
   Widget _getSearchOptions() {
     return Column(
       children: [
-        Container(
-          margin: const EdgeInsets.only(top: 10, bottom: 0),
-          child: Row(
-            children: [
-              customWidget.getBoldText("Latest", size: 24),
-            ],
-          ),
-        ),
         SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(children: [
-            for (String item in [
-              "News",
-              "Topics",
-              "Source",
-            ])
-              Row(
+            for (SearchOptions option in options)
+              Column(
                 children: [
-                  TextButton(
-                    child: customWidget.blurredText(item),
-                    onPressed: () {},
+                  Row(
+                    children: [
+                      TextButton(
+                        child: customWidget.blurredText(option.type ?? ""),
+                        onPressed: () {
+                          setState(() {
+                            activeOption = option;
+                          });
+                        },
+                      ),
+                      const SizedBox(width: 3)
+                    ],
                   ),
-                  const SizedBox(width: 3)
+                  Transform.translate(
+                    offset: const Offset(-0, -14),
+                    child: Container(
+                      height: option.type == activeOption.type ? 3 : 0,
+                      width: option.type == activeOption.type
+                          ? "${activeOption.type}".length * 13
+                          : 0,
+                      color: Colors.blue,
+                    ),
+                  )
+
                 ],
               )
           ]),
@@ -96,14 +125,15 @@ class _SearchPageState extends State<SearchPage> {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
-          customWidget.getMediumImage("assets/images/6.jpg"),
+          customWidget.getMediumImage("assets/images/6.jpg", size: 60),
           const SizedBox(width: 10),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               customWidget.blurredText("Europe"),
-              customWidget.getNormalText("Ukraine's president, Zelenskey says",
-                  size: 16),
+              customWidget.getNormalText(
+                  "${"Ukraine's president, Zelenskey says".substring(0, 10)}...",
+                  size: 10),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
@@ -130,6 +160,7 @@ class _SearchPageState extends State<SearchPage> {
               ),
             ],
           ),
+          customWidget.getElevatedBtn(context, activeOption.task ?? "")
         ],
       ),
     );
