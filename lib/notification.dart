dd6654f54609693cf16c6e734176857e8081839d
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-09 00:20:21 +0300

    added dot displayer

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:technews/custom_date.dart';
 
 class Notifications extends StatefulWidget {
   const Notifications({super.key});
@@ -7,16 +8,23 @@ class Notifications extends StatefulWidget {
   State<Notifications> createState() => _NotificationState();
 }
 
+String dotDisplayer(String text) {
+  int textLength = text.length;
+  if (textLength > 20) {
+    return '${text.substring(0, 19)}...';
+  } else {
+    return text;
+  }
+}
+
 class _NotificationState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const SizedBox(
-          child: Text(
-            'Notifications',
-            textAlign: TextAlign.center,
-          ),
+        title: const Text(
+          'Notifications',
+          textAlign: TextAlign.center,
         ),
         leading: IconButton(
           icon: const Icon(
@@ -28,14 +36,66 @@ class _NotificationState extends State<Notifications> {
           },
         ),
         actions: [
-            IconButton(
-              icon: const Icon(
-                Icons.more_vert,
-              ),
-            onPressed: () {
-              
-            },
-            ),        ],
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              padding: const EdgeInsets.only(left: 0),
+              child: CustomDateDisplay.getDate(context)[0]),
+          SingleChildScrollView(
+            // Wrap the Row with SingleChildScrollView
+            scrollDirection:
+                Axis.horizontal, // Set the scroll direction to horizontal
+            child: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 4, color: Colors.white),
+                    shape: BoxShape.circle,
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/bbc.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 0),
+                      child: Text(
+                        dotDisplayer(
+                          'Europe Europe: Lorem, ipsum dolor sit amet consect etur adipisicing elit.Lorem, ipsum dolor ipsum dolor',
+                        ),
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    const Text(
+                      '4h ago',
+                      textAlign: TextAlign.left,
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Follow'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
