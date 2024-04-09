b309323b56882a9c51961b6fd5b5fa96e0e80679
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-09 23:38:22 +0300

    edited the welcome text in welcome page

--- lib/App.dart
+++ lib/App.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:technews/custom_widget.dart';
 import 'package:technews/logo.dart';
 
 class MainApp extends StatefulWidget {
@@ -20,11 +21,43 @@ class _MainAppState extends State<MainApp> {
               const Logo(),
               const SizedBox(height: 20),
               const SizedBox(height: 20),
-              const Text(
-                'A place to stay up to date with the latest tech news',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w300,
+              Container(
+                margin: const EdgeInsets.all(20),
+                // child: const Text(
+                //   'A place to stay up to date with the latest tech news.',
+                //   textAlign: TextAlign.center,
+                //   style: TextStyle(
+                //     fontSize: 18,
+                //     fontWeight: FontWeight.w300,
+                //   ),
+                // ),
+                child: RichText(
+                  text: const TextSpan(
+                    text: 'A place to ',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.blueGrey),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'stay up to date ',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueGrey)),
+                      TextSpan(
+                        text: ' with the ',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.blueGrey),
+                      ),
+                      TextSpan(
+                          text: 'latest tech news. ',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueGrey)),
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(height: 20),
@@ -32,11 +65,17 @@ class _MainAppState extends State<MainApp> {
                 onPressed: () {
                   Navigator.pushNamed(context, "/on_boarding");
                 },
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    backgroundColor: Colors.blueAccent),
                 child: const Text(
                   'Get Started',
                   style: TextStyle(
                     fontSize: 15,
                     fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 250, 254, 253),
                   ),
                 ),
               ),
