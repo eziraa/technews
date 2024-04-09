b7ad7d3c4d3dba43f7232d71a27e8c18c8940d3b
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-09 22:25:58 +0300

    added body to change password

--- lib/change_password.dart
+++ lib/change_password.dart
@@ -26,7 +26,28 @@ class _ChangePasswordState extends State<ChangePassword> {
           },
         ),
       ),
-     
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        child: const Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Enter Old Password", labelStyle: TextStyle()),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Enter New Password", labelStyle: TextStyle()),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Re-enter New Password", labelStyle: TextStyle()),
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
     );
   }
 }
