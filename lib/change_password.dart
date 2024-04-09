67d5c8565123c46132882c917fb08da0f521c244
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-09 22:25:39 +0300

    added appbar for change password

---
+++ lib/change_password.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ChangePassword extends StatefulWidget {
+  const ChangePassword({super.key});
+
+  @override
+  State<ChangePassword> createState() => _ChangePasswordState();
+}
+
+class _ChangePasswordState extends State<ChangePassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Change Password',
+          textAlign: TextAlign.center,
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+     
+    );
+  }
+}
