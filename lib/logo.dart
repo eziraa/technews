0aba37f4e5dce9b8eea4771137f1a4ee8a285f6c
eziraa <ezratgab@gmail.com>
2024-04-10 20:23:22 +0300

    updating the custom widget to to have resizable logo

--- lib/logo.dart
+++ lib/logo.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-class Logo extends StatelessWidget {
-  const Logo({super.key});
+class Logo {
 
-  @override
-  Widget build(BuildContext context) {
+  Widget getLogo({double size = 140}) {
     return Container(
       padding: const EdgeInsets.only(top: 20),
       child: Row(
@@ -12,8 +10,16 @@ class Logo extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           // Image.asset('assets/images/logo.png'),
-          const CircleAvatar(
-            child: Image(image: AssetImage("assets/images/logo.png")),
+          Container(
+            width: size,
+            height: size,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.asset(
+              'assets/images/logo.png',
+              fit: BoxFit.cover,
+            ),
           ),
           const SizedBox(width: 10),
           RichText(
