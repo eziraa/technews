4db96d2cc9c99942aebf146dff16afa26237978e
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-10 21:46:02 +0300

    handled overflow with singlechildscrollview

--- lib/verify_email.dart
+++ lib/verify_email.dart
@@ -16,72 +16,67 @@ class _VerifyEmailState extends State<VerifyEmail> {
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
       ),
-      body: Container(
-        margin: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Logo().getLogo(size: 70),
-            const SizedBox(height: 30),
-            Container(
-              margin: const EdgeInsets.only(top: 50),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      const Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: "Enter Your Email",
-                            labelStyle: TextStyle(),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      // ElevatedButton(
-                      //   onPressed: null,
-                      //   child: Text("Get OTP",
-                      //       style: TextStyle(
-                      //         fontSize: 14,
-                      //         color: Colors.white,
-                      //       )),
-                      // ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25),
-                            ),
-                            backgroundColor: Colors.blueAccent),
-                        child: const Text(
-                          'Get OTP',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 250, 254, 253),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: "Verification Code",
-                      labelStyle: TextStyle(),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                ],
-              ),
-            ),
-            Container(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Logo().getLogo(size: 70),
+              const SizedBox(height: 30),
+              Container(
                 margin: const EdgeInsets.only(top: 50),
-                child: CustomWidget.getElevatedBtn(
-                  context,
-                  'Verify',
-                  handler: () => {Navigator.pushNamed(context, "/forgot_password")},
-                )),
-          ], // Closing square bracket for Column children
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              labelText: "Enter Your Email",
+                              labelStyle: TextStyle(),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              backgroundColor: Colors.blueAccent),
+                          child: const Text(
+                            'Get OTP',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 250, 254, 253),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    const TextField(
+                      decoration: InputDecoration(
+                        labelText: "Verification Code",
+                        labelStyle: TextStyle(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 50),
+                  child: CustomWidget.getElevatedBtn(
+                    context,
+                    'Verify',
+                    handler: () => {Navigator.pushNamed(context, "/forgot_password")},
+                  )),
+            ], // Closing square bracket for Column children
+          ),
         ),
       ), // Closing parenthesis for Container
     ); // Closing parenthesis for Scaffold
