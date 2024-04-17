c4747ee621ed68d19cb403337c300ce21287c729
NetsanetAlemu <netsialemu1907@gmail.com>
2024-04-17 20:32:52 +0300

    connected firebase with techbuzz

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,14 @@ import 'package:technews/latest.dart';
 import 'package:technews/notification.dart';
 import 'package:technews/verify_email.dart';
 import 'package:technews/select_news_source.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
 
   runApp(
     GetMaterialApp(
