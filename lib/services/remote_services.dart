7ee29ca73bc145e2b684868d3336941e0f78973a
eziraa <ezratgab@gmail.com>
2024-04-26 19:48:50 +0300

    updating the remote services

--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -7,13 +7,12 @@ class RemoteServices {
   static var client = http.Client();
   static Future<List<News>> fetchTechNews() async {
     var uri = Uri.parse(
-        'https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=f78ba5b9d1f34e279c7e8b41654a846d');
+        'https://newsapi.org/v2/top-headlines?category=technology&pageSize=40&language=en&apiKey=f78ba5b9d1f34e279c7e8b41654a846d');
     var response = await client.get(uri);
     if (response.statusCode == 200) {
       var jsonString = response.body;
       var jsonObject = jsonDecode(jsonString);
       var newsJsonArray = jsonObject['articles'];
-      print(jsonEncode(newsJsonArray));
       return newsFromJson(jsonEncode(newsJsonArray));
     } else {
       return Future.error(response.statusCode);
