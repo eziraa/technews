7865151e4f0d77093c509a87f96468b528727855
eziraa <ezratgab@gmail.com>
2024-04-26 14:49:30 +0300

    Adding remote services to fetch data

---
+++ lib/services/remote_services.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:technews/model/news_model.dart';
+
+class RemoteServices {
+  static var client = http.Client();
+  static Future<List<News>> fetchTechNews() async {
+    var uri = Uri.parse(
+        'https://newsapi.org/v2/top-headlines?category=technology&language=en&apiKey=f78ba5b9d1f34e279c7e8b41654a846d');
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      var jsonObject = jsonDecode(jsonString);
+      var newsJsonArray = jsonObject['articles'];
+      print(jsonEncode(newsJsonArray));
+      return newsFromJson(jsonEncode(newsJsonArray));
+    } else {
+      return Future.error(response.statusCode);
+    }
+  }
+}
