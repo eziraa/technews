6d56f9ed68df73f4ad2f4c9b484c2a8049107122
eziraa <ezratgab@gmail.com>
2024-04-28 18:31:24 +0300

    creating method to add user to our firebase database

---
+++ lib/services/database.dart
@@ -0,0 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseMethods {
+  Future addUser(String userId, Map<String, dynamic> userInfoMap) {
+    return FirebaseFirestore.instance
+        .collection("User")
+        .doc(userId)
+        .set(userInfoMap);
+  }
+}
