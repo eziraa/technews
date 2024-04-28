9b8a6881b46bffbcb618ed913a228362e267ea64
eziraa <ezratgab@gmail.com>
2024-04-28 18:37:42 +0300

    implementing the forgot password functionality

--- lib/forgot_password.dart
+++ lib/forgot_password.dart
@@ -1,6 +1,6 @@
+import 'package:technews/signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:technews/custom_widget.dart';
-import 'package:technews/logo.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
@@ -10,52 +10,162 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
+  String email = "";
+  TextEditingController mailcontroller = new TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
+
+  resetPassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+        "Password Reset Email has been sent !",
+        style: TextStyle(fontSize: 20.0),
+      )));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text(
+          "No user found for that email.",
+          style: TextStyle(fontSize: 20.0),
+        )));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Forgot Password',
-          style: TextStyle(
-            color: Color.fromARGB(255, 250, 254, 253),
-          ),
-        ),
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: Column(
-        children: [
-          Logo().getLogo(),
-          Container(
-            margin: const EdgeInsets.all(30),
-            child: const Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Username", labelStyle: TextStyle()),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Email", labelStyle: TextStyle()),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Enter Your password", labelStyle: TextStyle()),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Re-enter your password", labelStyle: TextStyle()),
-                ),
-                SizedBox(height: 10),
-              ],
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 70.0,
             ),
-          ),
-          Container(
-              margin: const EdgeInsets.all(20),
-              child: CustomWidget.getElevatedBtn(context, 'Sign in')),
-        ],
+            Container(
+              alignment: Alignment.topCenter,
+              child: const Text(
+                "Password Recovery",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Text(
+              "Enter your mail",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: Form(
+                key: _formkey,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: ListView(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white70, width: 2.0),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please Enter Email';
+                            }
+                            return null;
+                          },
+                          controller: mailcontroller,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: const InputDecoration(
+                              hintText: "Email",
+                              hintStyle: TextStyle(
+                                  fontSize: 18.0, color: Colors.white),
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Colors.white70,
+                                size: 30.0,
+                              ),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          if (_formkey.currentState!.validate()) {
+                            setState(() {
+                              email = mailcontroller.text;
+                            });
+                            resetPassword();
+                          }
+                        },
+                        child: Container(
+                          width: 140,
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: const Center(
+                            child: Text(
+                              "Send Email",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't have an account?",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                          const SizedBox(
+                            width: 5.0,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const SignUp()));
+                            },
+                            child: const Text(
+                              "Create",
+                              style: TextStyle(
+                                  color: Color.fromARGB(225, 184, 166, 6),
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
