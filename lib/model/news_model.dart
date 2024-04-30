7885e4e59288799035bbeebd6aa07b83dc746a9a
a-abrham <abrshkorra@gmail.com>
2024-04-30 14:24:43 +0300

    update

--- lib/model/news_model.dart
+++ lib/model/news_model.dart
@@ -50,3 +50,22 @@ class News {
       };
 }
 
+class Source {
+  String? id;
+  String name;
+
+  Source({
+    required this.id,
+    required this.name,
+  });
+
+  factory Source.fromJson(Map<String, dynamic> json) => Source(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+      };
+}
