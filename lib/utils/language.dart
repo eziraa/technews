6d713f3a5d7449ecb7fe423768e945eb415da2ec
eziraa <ezratgab@gmail.com>
2024-04-30 12:20:12 +0300

    creating the Language class and to have list language and according to them to display the list of user preference language

---
+++ lib/utils/language.dart
@@ -0,0 +1,23 @@
+class Language {
+  final String short;
+  final String long;
+
+  Language({required this.short, required this.long});
+}
+
+List<Language> languages = [
+  Language(short: 'ar', long: 'Arabic'),
+  Language(short: 'de', long: 'German'),
+  Language(short: 'en', long: 'English'),
+  Language(short: 'es', long: 'Spanish'),
+  Language(short: 'fr', long: 'French'),
+  Language(short: 'he', long: 'Hebrew'),
+  Language(short: 'it', long: 'Italian'),
+  Language(short: 'nl', long: 'Dutch'),
+  Language(short: 'no', long: 'Norwegian'),
+  Language(short: 'pt', long: 'Portuguese'),
+  Language(short: 'ru', long: 'Russian'),
+  Language(short: 'sv', long: 'Swedish'),
+  Language(short: 'ud', long: 'Urdu'),
+  Language(short: 'zh', long: 'Chinese'),
+];
