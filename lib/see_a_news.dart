f6aebb21a68b3c0cc4c07b30c918d3c25f2f823e
eziraa <ezratgab@gmail.com>
2024-05-01 15:18:53 +0300

    updating the see news page display an icon button if saved to remove if not to save the news

--- lib/see_a_news.dart
+++ lib/see_a_news.dart
@@ -19,68 +19,81 @@ class SeeNewsDetailPage extends StatefulWidget {
 }
 
 class _SeeNewsDetailPageState extends State<SeeNewsDetailPage> {
+  final NewsController _newsController = Get.put(NewsController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomWidget.getAppBarWithShare(context, news: widget.news),
-      body: Column(
-        children: [
-          Container(
-              padding: const EdgeInsets.all(10),
+    return Obx(
+      () => Scaffold(
+        appBar: CustomWidget.getAppBarWithShare(context, news: widget.news),
+        body: Column(
+          children: [
+            Container(
+                padding: const EdgeInsets.all(10),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Row(
+                      children: [
+                        CustomWidget.getProfileImage(widget.news!.source.name,
+                            size: 85),
+                        const SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomWidget.getBoldText(widget.news!.source.name),
+                            CustomWidget.blurredText(
+                                "${timeAgo(widget.news!.publishedAt.toString())}",
+                                size: 12),
+                          ],
+                        ),
+                        Expanded(
+                          child: Container(),
+                        ),
+                        _newsController.savedNews.contains(widget.news!)
+                            ? IconButton(
+                                onPressed: () {
+                                  _newsController
+                                      .removeNewsFromSaved(widget.news!);
+                                },
+                                icon: const Icon(Icons.bookmark),
+                                color: Colors.red,
+                              )
+                            : IconButton(
+                                onPressed: () {
+                                  _newsController.saveNews(widget.news!);
+                                },
+                                icon: const Icon(Icons.bookmark_border),
+                                color: Colors.red,
+                                // color: Theme.of(context).primaryColor,
+                              ),
+                      ],
+                    ),
+                  ),
+                )),
+            Expanded(
               child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
+                scrollDirection: Axis.vertical,
                 child: Container(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: Row(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CustomWidget.getProfileImage(widget.news!.source.name,
-                          size: 85),
-                      const SizedBox(width: 10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CustomWidget.getBoldText(widget.news!.source.name),
-                          CustomWidget.blurredText(
-                              "${timeAgo(widget.news!.publishedAt.toString())}",
-                              size: 12),
-                        ],
-                      ),
-                      Expanded(
-                        child: Container(),
-                      ),
-                      CustomWidget.getElevatedBtn(context, "Save",
-                          size: 14,
-                          handler: () => {
-                                Get.find<NewsController>()
-                                    .saveNews(widget.news ?? "" as News)
-
-                              })
+                      CustomWidget.getNetWorkImage(
+                          context, widget.news!.urlToImage ?? ""),
+                      const SizedBox(height: 10),
+                      // CustomWidget.blurredText("Europe"),
+                      CustomWidget.getBoldText(
+                          widget.news?.title ?? "There is no news title"),
+                      getNewsBody(),
                     ],
                   ),
                 ),
-              )),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CustomWidget.getNetWorkImage(
-                        context, widget.news!.urlToImage ?? ""),
-                    const SizedBox(height: 10),
-                    // CustomWidget.blurredText("Europe"),
-                    CustomWidget.getBoldText(
-                        widget.news?.title ?? "There is no news title"),
-                    getNewsBody(),
-                  ],
-                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
