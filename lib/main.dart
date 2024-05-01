beecd19ce5da81277f82e2933d0bb2ed94482449
eziraa <ezratgab@gmail.com>
2024-05-01 08:06:31 +0300

    fixing related to the dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:technews/about.dart';
+import 'package:technews/controller/theme_controller.dart';
 import 'package:technews/firebase_options.dart';
+import 'package:technews/home_page.dart';
 import 'package:technews/login.dart';
 import 'package:technews/news_channel.dart';
 import 'package:technews/on_boarding_1.dart';
@@ -11,28 +14,43 @@ import 'package:technews/trending.dart';
 import 'package:technews/latest.dart';
 import 'package:technews/select_news_source.dart';
 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final ThemeController themeController = Get.put(ThemeController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => GetMaterialApp(
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        themeMode: themeController.themeMode.value,
+        initialRoute: '/',
+        getPages: [
+          GetPage(name: '/', page: () => HomePage()),
+          GetPage(name: '/on_boarding', page: () => const OnBoarding1()),
+          GetPage(name: '/log_in', page: () => const LogIn()),
+          GetPage(name: '/trending', page: () => const Trending()),
+          GetPage(name: '/latest', page: () => const LatestPage()),
+          GetPage(name: '/search', page: () => const SearchPage()),
+          GetPage(name: '/sign_up', page: () => const SignUpPage()),
+          GetPage(name: '/select_news_source', page: () => const NewsSource()),
+          GetPage(name: '/news_channel', page: () => const NewsChannel()),
+          GetPage(name: '/about', page: () => AboutPage()),
+        ],
+      ),
+    );
+  }
+}
+
 Future<void> main() async {
   // await dotenv.dotenv.load(fileName: '.env');
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(
-    GetMaterialApp(
-      title: 'Tech News App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.grey),
-      initialRoute: '/',
-      getPages: [
-        GetPage(name: '/', page: () => const HomePage()),
-        GetPage(name: '/on_boarding', page: () => const OnBoarding1()),
-        GetPage(name: '/log_in', page: () => const LogIn()),
-        GetPage(name: '/trending', page: () => const Trending()),
-        GetPage(name: '/latest', page: () => const LatestPage()),
-        GetPage(name: '/search', page: () => const SearchPage()),
-        GetPage(name: '/sign_up', page: () => const SignUpPage()),
-        GetPage(name: '/select_news_source', page: () => const NewsSource()),
-        GetPage(name: '/news_channel', page: () => const NewsChannel()),
-      ],
-    ),
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
 }
