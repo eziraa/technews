9cce26a4d174c444b995604262f0c294ef5818b0
eziraa <ezratgab@gmail.com>
2024-05-01 16:35:03 +0300

    updating the user controller

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -6,6 +6,7 @@ class UserController extends GetxController {
     id: "1",
     name: "Anonymous",
     email: "exmaple@technews.com",
+          phoneNumber: "89053656"
   ).obs;
   User get user => _user.value;
 
@@ -16,10 +17,15 @@ class UserController extends GetxController {
       id: "1",
       name: "Anonymous",
       email: "exmaple@technews.com",
+        phoneNumber: "89053656"
     );
   }
 
   void updateUser(User newUser) {
+    _user.value = newUser;
+  }
+
+  void setCurrentUser(User newUser) {
     _user.value = newUser;
   }
 }
