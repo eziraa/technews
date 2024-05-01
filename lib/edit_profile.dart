fda3e37aef137b8cbc1bb31e4303fafb8b4c9f03
eziraa <ezratgab@gmail.com>
2024-05-01 16:48:14 +0300

    updating the edit profile to use the dark mode

--- lib/edit_profile.dart
+++ lib/edit_profile.dart
@@ -1,4 +1,14 @@
+import 'dart:html';
+import 'dart:html';
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:technews/controller/theme_controller.dart';
+import 'package:technews/custom_widget.dart';
+import 'package:technews/services/remote_services.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
@@ -8,9 +18,11 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  final ThemeController themeController = Get.put(ThemeController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Obx(
+      () => Scaffold(
         appBar: AppBar(
           title: const Text(
             'Edit your profile',
@@ -19,7 +31,6 @@ class _EditProfileState extends State<EditProfile> {
           leading: IconButton(
             icon: const Icon(
               Icons.arrow_back,
-              color: Colors.black,
             ),
             onPressed: () => {Navigator.pop(context)},
           ),
@@ -35,89 +46,100 @@ class _EditProfileState extends State<EditProfile> {
         body: Container(
           padding: const EdgeInsets.only(left: 15, top: 20, right: 15),
           child: GestureDetector(
-              onTap: () {
-                FocusScope.of(context).unfocus();
-              },
-              child: ListView(
-                children: [
-                  Center(
-                      child: Stack(children: [
-                    Container(
-                      width: 130,
-                      height: 130,
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 4, color: Colors.white),
-                          boxShadow: [
-                            BoxShadow(
-                                spreadRadius: 2,
-                                blurRadius: 10,
-                                color: Colors.black.withOpacity(0.1)),
-                          ],
-                          shape: BoxShape.circle,
-                          image: const DecorationImage(
-                            image: AssetImage(
-                                'assets/images/profile.jpg'),
-                            fit: BoxFit.cover,
-                          )),
-                    ),
-                    Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(width: 4, color: Colors.white),
-                              color: Colors.blue),
-                          child: const Icon(
-                            Icons.edit,
-                            color: Colors.white,
-                          ),
-                        ))
-                  ])),
-                  const SizedBox(height: 30),
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: ListView(
+              children: [
+                Center(
+                    child: Stack(children: [
                   Container(
-                    margin: const EdgeInsets.only(top: 50),
-                    child: const Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "Username",
-                        labelStyle: TextStyle(),
+                    width: 130,
+                    height: 130,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 4, color: Colors.white),
+                        boxShadow: [
+                          BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1)),
+                        ],
+                        shape: BoxShape.circle,
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/profile.jpg'),
+                          fit: BoxFit.cover,
+                        )),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 4, color: Colors.white),
+                        color: Colors.blue,
                       ),
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.white,
                         ),
-                        SizedBox(height: 10),
-                        SizedBox(height: 10),
-                        TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "FullName",
-                              labelStyle: TextStyle()),
-                        ),
-                        SizedBox(height: 10),
-                        TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "Email",
-                              labelStyle: TextStyle()),
-                        ),
-                        SizedBox(height: 10),
-                        TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "Phone Number",
-                        labelStyle: TextStyle(),
+                        onPressed: () async {
+                          final ImagePicker _picker = ImagePicker();
+                          // Pick an image
+                          // final XFile? image = await _picker.pickImage(
+                          //     source: ImageSource.gallery);
+                        },
                       ),
-                        ),
-                        SizedBox(height: 10),
-                      ],
                     ),
                   ),
-                ],
+                ])),
+                const SizedBox(height: 30),
+                Container(
+                  margin: const EdgeInsets.only(top: 50),
+                  child: const Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Username",
+                          labelStyle: TextStyle(),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "FullName",
+                            labelStyle: TextStyle()),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Email",
+                            labelStyle: TextStyle()),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Phone Number",
+                          labelStyle: TextStyle(),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
+        bottomNavigationBar: CustomWidget.getBottomNavBar(context, 3),
       ),
     );
   }
