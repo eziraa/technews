ad23fb358617b179028ec4e91840c12003b8f73e
eziraa <ezratgab@gmail.com>
2024-05-01 08:19:51 +0300

    updating the custom drawer widget to  implement the mode mode and light mode of the app

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:technews/controller/theme_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:technews/controller/theme_controller.dart';
 
-class CustomDrawer {
-  static Widget buildDrawer(BuildContext context) {
+class CustomDrawer extends StatefulWidget {
+  @override
+  _CustomDrawerState createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  final ThemeController themeController = Get.put(ThemeController());
+
+  @override
+  Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -27,47 +41,83 @@ class CustomDrawer {
               ),
             ),
           ),
-
           ListTile(
-            title: Text('Trending'),
-            onTap: () {
-              Navigator.pushNamed(context, '/trending');
-            },
-          ),
-          ListTile(
+            leading: Icon(Icons.new_releases), // Add an icon
             title: Text('Latest'),
             onTap: () {
-              Navigator.pushNamed(context, '/latest');
+              Get.toNamed('/latest');
             },
           ),
           ListTile(
+            leading: Icon(Icons.search), // Add an icon
             title: Text('Search'),
             onTap: () {
-              Navigator.pushNamed(context, '/search');
+              Get.toNamed('/search');
             },
           ),
           ListTile(
+            leading: Icon(Icons.person), // Add an icon
             title: Text('Profile'),
             onTap: () {
-              Navigator.pushNamed(context, '/profile');
+              Get.toNamed('/profile');
             },
           ),
           ListTile(
+            leading: Icon(Icons.settings), // Add an icon
             title: Text('Settings'),
             onTap: () {
-              Navigator.pushNamed(context, '/settings');
+              Get.toNamed('/settings');
             },
           ),
           ListTile(
-            title: Text('Dark Mode'),
+            leading: Icon(Icons.dark_mode),
+            title: Text('Change Theme'),
             onTap: () {
-              // Implement your logic for toggling dark mode
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Change Theme'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        GetBuilder<ThemeController>(
+                          init: ThemeController(),
+                          builder: (controller) => Column(
+                            children: ThemeMode.values.map((mode) {
+                              return RadioListTile<ThemeMode>(
+                                title: Text(mode.toString().split('.').last),
+                                value: mode,
+                                groupValue: controller.themeMode.value,
+                                onChanged: (ThemeMode? mode) {
+                                  controller
+                                      .changeMode(mode ?? ThemeMode.system);
+                                  Navigator.pop(context);
+                                },
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ],
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('Close'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
             },
           ),
           ListTile(
+            leading: Icon(Icons.info), // Add an icon
             title: Text('About'),
             onTap: () {
-              Navigator.pushNamed(context, '/about');
+              Get.toNamed('/about');
             },
           ),
           // Add more ListTiles for other menu items
