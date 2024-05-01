55d65648b064adf197065edbc796f256a323b185
eziraa <ezratgab@gmail.com>
2024-05-01 10:34:32 +0300

    Creating user controller

---
+++ lib/controller/user_controller.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+import 'package:technews/model/user_model.dart';
+
+class UserController extends GetxController {
+  var _user = User(
+    id: "1",
+    name: "Anonymous",
+    email: "exmaple@technews.com",
+  ).obs;
+  User get user => _user.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _user.value = User(
+      id: "1",
+      name: "Anonymous",
+      email: "exmaple@technews.com",
+    );
+  }
+
+  void updateUser(User newUser) {
+    _user.value = newUser;
+  }
+}
